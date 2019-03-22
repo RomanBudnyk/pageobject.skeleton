9b02790d6e5c7c21ca96378c8d5cfef5628837e8
Roman Budnyk <r.m.budnik@gmail.com>
2019-03-22 14:37:24 +0200

    commented cross-browser tests

--- src/main/java/com/pageobject/skeleton/BaseTest.java
+++ src/main/java/com/pageobject/skeleton/BaseTest.java
@@ -6,38 +6,34 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Optional;
-import org.testng.annotations.Parameters;
+import org.testng.annotations.*;
 
 public class BaseTest extends ConciseAPI {
 
     static WebDriver driver;
 
-//    static {
-//        WebDriverManager.firefoxdriver().setup();
-//        driver = new FirefoxDriver();
-//    }
-
-    @Parameters("browser")
-    @BeforeTest
-    public void setup(@Optional ("edge") String browser) throws Exception {
-        if (browser.equalsIgnoreCase("firefox")) {
-            WebDriverManager.firefoxdriver().setup();
-            driver = new FirefoxDriver();
-        } else if (browser.equalsIgnoreCase("chrome")) {
-            WebDriverManager.chromedriver().setup();
-            driver = new ChromeDriver();
-        } else if (browser.equalsIgnoreCase("edge")) {
-            WebDriverManager.edgedriver().setup();
-            driver = new EdgeDriver();
-        } else {
-            throw new Exception("Browser is not correct");
-        }
-        driver.get("https://google.com/ncr");
-
+    static {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
     }
+
+//    @Parameters("browser")
+//    @BeforeMethod
+//    public void setup(@Optional ("chrome") String browser) throws Exception {
+//        if (browser.equalsIgnoreCase("firefox")) {
+//            WebDriverManager.firefoxdriver().setup();
+//            driver = new FirefoxDriver();
+//        } else if (browser.equalsIgnoreCase("chrome")) {
+//            WebDriverManager.chromedriver().setup();
+//            driver = new ChromeDriver();
+//        } else if (browser.equalsIgnoreCase("edge")) {
+//            WebDriverManager.edgedriver().setup();
+//            driver = new EdgeDriver();
+//        } else {
+//            throw new Exception("Browser is not correct");
+//        }
+//        driver.get("https://google.com/ncr");
+//    }
 
     @Override
     public WebDriver getWebDriver() {
--- src/main/java/com/pageobject/skeleton/TestHere.java
+++ src/main/java/com/pageobject/skeleton/TestHere.java
@@ -10,6 +10,8 @@ public class TestHere extends BaseTest {
 
     @Test
     public void someTest() {
+        driver.get("https://google.com/ncr");
+        System.out.println("some");
         driver.manage().timeouts().implicitlyWait(4, TimeUnit.SECONDS);
         driver.findElement(By.name("q")).sendKeys("hi", Keys.ENTER);
     }
